96629db93d6f8d263b179effc151e521043d5747
strawberryCheeseCake2 <gu7991@naver.com>
2024-06-21 16:44:37 +0900

    Modified create_user to always return user, not raising error

--- user.py
+++ user.py
@@ -24,5 +24,6 @@ router = APIRouter()
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_name(db, name=user.name)
     if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
+        return db_user
+        # raise HTTPException(status_code=400, detail="DUPLICATE_USERNAME")
     return crud.create_user(db=db, user=user)
