36bf63064ad6872a2ea3276eb200a9422f147da3
kimMinGyu <46309433+strawberryCheeseCake2@users.noreply.github.com>
2024-06-24 09:39:12 +0900

    Implemented AI chat logging feature

--- chat_socket.py
+++ chat_socket.py
@@ -15,8 +15,6 @@ from .devil import DevilManager
 
 class ConnectionManager:
     def __init__(self, devil: Optional[DevilManager] = None):
-        # init counter
-        # init client, assistant, thread
         self.active_connections: List[WebSocket] = []
         self.devil = devil
         self.counter = 0
@@ -48,6 +46,7 @@ connection_manager = ConnectionManager(devil)
 
 router = APIRouter()
 
+
 def get_db():
     db = SessionLocal()
     try:
@@ -56,6 +55,8 @@ def get_db():
         db.close()
 
 # https://github.com/tiangolo/fastapi/discussions/9664
+
+
 @router.on_event("startup")
 async def startup_event():
     await devil.setup_devil()
@@ -85,7 +86,6 @@ async def websocket_endpoint(websocket: WebSocket, username: str, db: Session = 
             print(json.dumps(message))
             await connection_manager.broadcast(json.dumps(message))
 
-            
             await connection_manager.devil.add_user_message(message["message"])
             print(connection_manager.counter)
             print(len(connection_manager.active_connections))
@@ -94,11 +94,17 @@ async def websocket_endpoint(websocket: WebSocket, username: str, db: Session = 
                     print("hit!")
                     devil_message_content = await connection_manager.devil.run_devill()
                     devil_message = {
-                        "sentTime": current_time, "username": "Annoymous Devil", "message": devil_message_content}
+                        "sentTime": current_time, "username": "Annoymous Devil", "message": devil_message_content
+                    }
+
+                    crud.log_message(db=db, message=schemas.MessageCreate(
+                        content=devil_message["message"],
+                        sender=devil_message["username"],
+                        sentTime=devil_message["sentTime"]
+                    ))
                     print(devil_message)
                     await connection_manager.broadcast(json.dumps(devil_message))
                 connection_manager.reset_counter()
-
 
     except WebSocketDisconnect:
         connection_manager.disconnect(websocket)
